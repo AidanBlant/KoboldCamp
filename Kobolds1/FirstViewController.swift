4c88d29fd2b20facf253caba14476cc74154de24
Aidan <aidanblant@gmail.com>
2020-08-23 18:45:13 -0700

    Initial Commit

---
+++ Kobolds1/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Kobolds1
+//
+//  Created by Aidan Blant on 8/23/20.
+//  Copyright © 2020 Aidan Blant. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Kobolds1/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Kobolds1
+//
+//  Created by Aidan Blant on 8/23/20.
+//  Copyright © 2020 Aidan Blant. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
